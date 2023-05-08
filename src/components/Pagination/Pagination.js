5180c930b57bb25133cc0f23a46221080ceac181
sonjamaichl <sonja.maichl@gmail.com>
2023-05-08 16:33:00 +0200

    eventHandler added to pagination to get value of clicked page

--- src/components/Pagination/Pagination.js
+++ src/components/Pagination/Pagination.js
@@ -1,12 +1,21 @@
 import * as React from 'react';
 import Pagination from '@mui/material/Pagination';
 import Stack from '@mui/material/Stack';
+import { Box } from '@mui/material';
+
+
+//eventHandler to get the value of the clicked page = pageNum
+function handleClick(event) {
+  console.log(event.target.innerText);  //nothing happens so far, change pageNum here!
+}
 
 export default function CardsPagination() {
   return (
+    <Box sx={{display: 'flex', justifyContent: 'center', mb: 4}}>
     <Stack spacing={2}>
-      <Pagination count={7} color="primary" />
+      <Pagination count={7} color="primary" onClick={(event) => handleClick(event)}/>
     </Stack>
+    </Box>
   );
 }
 
