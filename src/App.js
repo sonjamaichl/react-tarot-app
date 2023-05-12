6123b40f18a0e41e96531ff31f4efd8f61195e06
sonjamaichl <sonja.maichl@gmail.com>
2023-05-12 12:01:01 +0200

    new views and routes added for details & 404 page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react'
 import Wiki from './views/Wiki';
 import Home from './views/Home';
 import Reading from './views/Reading';
+import Details from './views/Details';
+import NotFound404 from './views/NotFound404';
 import { Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -11,7 +13,9 @@ function App() {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/wiki" element={<Wiki />} />
+      <Route path="/card/:id" element={<Details />} />
       <Route path="/reading" element={<Reading />} />
+      <Route path="*" element={<NotFound404 />} />
     </Routes>
   );
 };
---
+++ src/views/NotFound404.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function NotFound404() {
+  return (
+    <h1>404 Page Not Found: Go back to Home (put Link here)</h1>
+  )
+}
+
+export default NotFound404
