2840d357a732b4af9dd4e78548e9491d188c2008
sonjamaichl <sonja.maichl@gmail.com>
2023-05-17 14:09:17 +0200

    upright variable fixed

--- src/views/Reading1Card.js
+++ src/views/Reading1Card.js
@@ -8,7 +8,8 @@ import backImg from '../utils/backOfCardsImgSrc';
 function Reading1Card() {
 
   let [card, setCard] = useState();
-  
+  let [upright, setUpright] = useState();
+
   const getCardById = async () => {
     //fetching data from tarot API
    const url = `http://localhost:3000/random/1`; //think about writing custom useFetch Hook...
@@ -19,6 +20,7 @@ function Reading1Card() {
      setCard(result[0]) //result is an array that contains one single card object
      console.log('This is your random card:')
      console.log(result);
+    setUpright(randomBoolean());  //using return value of randomBoolean to decide if the card is upright or reversed
     } catch (error){
       console.log(error);
     }
@@ -28,13 +30,12 @@ function Reading1Card() {
     getCardById();
   }, []); //function will only run once at initial render, change dependency array if other behavior is needed
 
-  const upright = randomBoolean();  //using return value of randomBoolean to decide if the card is upright or reversed
-  console.log(upright);
+  
 
   let [clicked, setClicked] = useState(false);
   function handleClick() {
     console.log('card was clicked, now the img src should change...');
-    setClicked (true);
+    setClicked(true);
   }
 
   //let h2text = upright ? card.name : `${card.name} reversed`;
@@ -48,7 +49,7 @@ function Reading1Card() {
             <h2>{clicked? upright? `Your card: ${card.name}` : `Your card: ${card.name} reversed` : 'Click on the card to see it...'}</h2>    
         
             <Card className="details-card" variant="outlined" ratio="7/12" sx={{ m: 2 }}>
-              <img className={!upright && 'reversed'} src={clicked ? card.img_drive : backImg} alt={clicked ? card.name : "back side of tarot card"} onClick={handleClick}></img>
+              <img className={upright? '':'reversed'} src={clicked ? card.img_drive : backImg} alt={clicked ? card.name : "back side of tarot card"} onClick={handleClick}></img>
                 </Card>
 
         </Box>
