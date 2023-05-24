44e6f67f26d05cd9970e50368bb8b9eb7a1f2a46
sonjamaichl <sonja.maichl@gmail.com>
2023-05-24 16:18:46 +0200

    conditional rendering added for test purposes: reading is now only available to logged in users

--- src/views/Reading.js
+++ src/views/Reading.js
@@ -3,9 +3,15 @@ import { Box, Toolbar } from '@mui/material';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
 import { NavLink } from 'react-router-dom';
+import { AuthContext } from '../firebase_setup/AuthContext'; 
+import { useContext } from 'react';
 
 function Reading() {
-    return (
+
+  const {user} = useContext(AuthContext);
+  
+  return (
+    user ?  //conditional rendering just for test purpose... later reading will be available for all (logged in users & guests), but only logged in users will be able to save and comment their reading
         <Box component="main" sx={{ p: 3 }}>
         <Toolbar />
         <h1>READING</h1>
@@ -15,8 +21,8 @@ function Reading() {
       <Button component={NavLink} to='1' variant="contained">1 card reading</Button>
       <Button component={NavLink} to='3' variant="outlined">3 cards spread</Button>
         </Stack>
-
-        </Box>
+      </Box> :
+      <Box sx = {{m:10}}>Please login to use get a personal reading</Box>   
   )
 }
 
